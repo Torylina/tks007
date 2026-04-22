66c4b887e6cd6fe11e9de2f9e5b1f634a022f0f3
torilina <148883897+Torylina@users.noreply.github.com>
2026-04-22 11:51:30 +0300

    Refactor print statements for better readability

--- run_shadow.py
+++ run_shadow.py
@@ -8,16 +8,16 @@ from shadow.polyedr import Polyedr
 tk = TkDrawer()
 try:
     for name in ["ccc", "cube", "box", "king", "cow"]:
-        print("=============================================================")
+        print("=" * 60)
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
         poly = Polyedr(f"data/{name}.geom")
         poly.draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
-        # Печать характеристики полиэдра
         result = poly.good_edges_length_sum()
-        print(f"Сумма длин рёбер с «хорошими» концами и серединой: {result:.6f}")
+        print(
+            f"Сумма длин 'хороших' рёбер: {result:.6f}")
         print(f"Всего рёбер в полиэдре: {len(poly.edges)}")
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
